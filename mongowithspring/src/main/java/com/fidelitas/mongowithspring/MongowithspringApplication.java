10fc19447b6852b1645f49f4a42896421c73c0a5
=Geison <=geisonvm@gmail.com>
2019-07-06 09:06:37 -0600

    Initial commit

---
+++ mongowithspring/src/main/java/com/fidelitas/mongowithspring/MongowithspringApplication.java
@@ -0,0 +1,13 @@
+package com.fidelitas.mongowithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongowithspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongowithspringApplication.class, args);
+    }
+
+}
